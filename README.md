6825ffbc8efca2c45f0f438a3302c4711dfcdea3
lemiffe <michael.warner@showpad.com>
2017-07-24 22:55:25 +0200

    First version (testing chatbot)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # minecraft-bots
 
 An assortment of bots for use in Minecraft
+
+To deploy simply push to master
+
+To run a bot in the server, start it in a "screen" using `node script.js`
---
+++ bot-chatbot.js
@@ -0,0 +1,12 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+    host: "localhost",
+    username: "Spybot",
+    version: "1.12"
+});
+
+bot.on('chat', (username, message) => {
+    if (username === bot.username) return;
+    bot.chat(message)
+});
