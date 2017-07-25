942d386e873d0368b5cbd40f13bbfdfa002fa924
Arnaud Schlupp <arnaud.schlupp@gmail.com>
2017-07-25 17:13:41 +0200

    Make elias smarter! He can now walk to me!

--- bot-elias.js
+++ bot-elias.js
@@ -1,4 +1,5 @@
-const mineflayer = require('mineflayer')
+const mineflayer = require('mineflayer');
+const navigatePlugin = require('mineflayer-navigate')(mineflayer);
 
 const myName = "Elias";
 
@@ -8,9 +9,39 @@ const bot = mineflayer.createBot({
     version: "1.12"
 });
 
+navigatePlugin(bot);
+// bot.navigate.blocksToAvoid[132] = true; // avoid tripwire
+// bot.navigate.blocksToAvoid[59] = false; // ok to trample crops
+
 bot.on('chat', (username, message) => {
-	if (username === bot.username) return;
-	if (message.startsWith(myName)) {
-		bot.chat(message);
-	}
+    const target = bot.players[username].entity;
+
+    if (username === bot.username) return;
+    if (message.startsWith(myName)) {
+        if (message.endsWith('come')) {
+            bot.navigate.to(target.position);
+        } else if (message.endsWith('stop')) {
+            bot.navigate.stop();
+        }
+    }
 });
+
+bot.navigate.on('pathFound', function (path) {
+    bot.chat("found path. I can get there in " + path.length + " moves.");
+});
+
+bot.navigate.on('cannotFind', function (closestPath) {
+    bot.chat("unable to find path. getting as close as possible");
+    bot.navigate.walk(closestPath);
+});
+
+bot.navigate.on('arrived', function () {
+    bot.chat("I have arrived");
+});
+
+bot.navigate.on('interrupted', function () {
+    bot.chat("stopping");
+});
+
+
+
