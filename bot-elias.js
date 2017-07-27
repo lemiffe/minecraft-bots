232a6dfd2b6b037c8b926bf5b6b6a9d40bfcffef
Arnaud Schlupp <arnaud.schlupp@gmail.com>
2017-07-27 21:52:24 +0200

    Follow me Elias!

--- bot-elias.js
+++ bot-elias.js
@@ -1,48 +1,46 @@
 const mineflayer = require('mineflayer');
-const navigatePlugin = require('mineflayer-navigate')(mineflayer);
+const Vec3 = require('vec3').Vec3
 
 const botName = 'Elias';
 const bot = mineflayer.createBot({
-    host: 'localhost',
+    host: '35.190.221.44',
     username: botName,
-    version: '1.12'
+    version: '1.12',
 });
 
-navigatePlugin(bot);
-// bot.navigate.blocksToAvoid[132] = true; // avoid tripwire
-// bot.navigate.blocksToAvoid[59] = false; // ok to trample crops
+bot.spawnPoint = null;
 
 bot.on('chat', (username, message) => {
     const player = bot.players[username].entity;
 
     if (username === bot.username) return;
     if (message.startsWith(botName)) {
-        if (message.endsWith('come')) {
-            bot.navigate.to(player.position.minus(player.velocity));
-        } else if (message.endsWith('location')) {
-            bot.chat(mineflayer.Location.entity);
-        } else if (message.endsWith('stop')) {
-            bot.navigate.stop();
+        if (message.endsWith(`come`)) {
+            bot.entity.position = bot.players['arnaudschlupp'].entity.position;
         }
     }
 });
 
-bot.navigate.on('pathFound', function (path) {
-    bot.chat(`found path. I can get there in ${path.length}  moves.`);
-});
+bot.on('spawn', () => {
+    bot.chat('Hi dude!');
+})
 
-bot.navigate.on('cannotFind', function (closestPath) {
-    bot.chat('unable to find path. getting as close as possible');
-    bot.navigate.walk(closestPath);
-});
+bot.on('death', () => {
+    bot.chat('Urgh! See ya cruel world...');
+})
 
-bot.navigate.on('arrived', function () {
-    bot.chat('I have arrived');
-});
+let playerPosition = new Vec3(0, 0, 0);
+bot.on('entityMoved', (entity) => {
+    if (entity.type === 'player' && entity.username === 'arnaudschlupp') {
+        if (!playerPosition.equals(entity.position)) {
+            const delta = 1;
+            bot.entity.position.set(entity.position.x - delta, entity.position.y, entity.position.z - delta);
+            bot.lookAt(entity.position.offset(0, entity.height, 0));
+            playerPosition.set(entity.position.x, entity.position.y, entity.position.z);
+        }
+    }
+})
 
-bot.navigate.on('interrupted', function () {
-    bot.chat('stopping');
-});
-
-
-
+const random = (min = 1, max = 3) => {
+    return Math.abs(Math.floor(Math.random() * max - min));
+}
